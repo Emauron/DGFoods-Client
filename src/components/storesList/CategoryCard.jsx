aeaee17fefdd8979293ba596eb02f3b2fa5c6ca7
Emauron Garcia Novaes <emauron.novaes@mse.com.br>
2025-09-07 18:22:42 -0300

    feito o header da loja

---
+++ src/components/storesList/CategoryCard.jsx
@@ -0,0 +1,20 @@
+export default function CategoryCard({ name }) {
+    // selected ? "ring-2 ring-gray-900/20" : ""
+    return (
+        <li>
+            <button
+            data-categoria={name}
+            className={`group w-full overflow-hidden rounded-xl border border-gray-200/80 bg-white text-left shadow-sm hover:shadow-md focus-visible:ring-2 focus-visible:ring-gray-900/20
+            `}
+            >
+            <div className="relative aspect-[4/3] bg-gray-100">
+                <div className="absolute inset-0 grid place-items-center text-gray-400">Imagem</div>
+                {/* Substitua por <img src="/caminho.jpg" alt="" className="h-full w-full object-cover" /> */}
+            </div>
+            <div className="p-3">
+                <p className="text-sm font-medium text-gray-900 group-hover:underline">{name}</p>
+            </div>
+            </button>
+        </li>
+    );
+}
--- src/pages/StoresList.jsx
+++ src/pages/StoresList.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-
+import Header from '../components/storesList/header'
+import SearchBar from '../components/storesList/SearchBar'
+import StoresCategories from '../components/storesList/StoresCategories'
 export default function StoresLists() {
     const { id_city } = useParams();
     return (
         <div>
-            <h1>Stores Lists</h1>
-            <p>Id City: {id_city}</p>
-
+            <Header id_city={id_city} />
+            <section className="mx-auto max-w-7xl px-3 sm:px-4 md:px-6 mt-4">
+                <SearchBar id_city={id_city} />
+            </section>
         </div>
     )
 }
